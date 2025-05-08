161e53ca313010da55d8dc17635dcad1ee78c782
Lyall <me@lyall.lol>
2025-05-08 23:10:07 +0100

    no quet

--- utils/matchUrl.js
+++ utils/matchUrl.js
@@ -3,7 +3,7 @@ const escapeRegex = require("./escapeRegex");
 function matchUrl(pattern, url) {
     // TODO: make better, /hello/* matches /hello/some/thing
     // Match URL, ignores trailing slash and duplicate slashes
-    return new RegExp(`^${pattern.split("*").map(escapeRegex).join(".+")}\/?$`).test(url.replace(/\/+/g, "/"));
+    return new RegExp(`^${pattern.split("*").map(escapeRegex).join(".+")}\/?$`).test(url.split("?")[0].replace(/\/+/g, "/"));
 }
 
 module.exports = matchUrl;
